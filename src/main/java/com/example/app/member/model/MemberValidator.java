4ee3e6ca90d128eefeeae4aa0bd5806555ff5877
Polar-Beary-Poby <primekk3502@gmail.com>
2026-04-03 18:45:36 +0900

    MemberValidator

---
+++ src/main/java/com/example/app/member/model/MemberValidator.java
@@ -0,0 +1,27 @@
+package com.example.app.member.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class MemberValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Member.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Member member = (Member) target;
+
+        String pw1 = member.getPassword();
+        String pw2 = member.getPassword2();
+        if (pw1 != null && pw1.equals(pw2)) {
+            //pass
+        } else {
+            errors.rejectValue("password2", "PASSWORD_NOT_EQULAS", "비밀번호 확인이 다릅니다");
+        }
+    }
+}
