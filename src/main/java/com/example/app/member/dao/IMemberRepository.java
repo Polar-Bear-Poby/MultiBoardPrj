d7205b5f941f4b1ee5d29a145ce1f0274abc5738
Polar-Beary-Poby <primekk3502@gmail.com>
2026-04-06 10:38:35 +0900

    Insert files

--- src/main/java/com/example/app/member/dao/IMemberRepository.java
+++ src/main/java/com/example/app/member/dao/IMemberRepository.java
@@ -3,9 +3,13 @@ package com.example.app.member.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.example.app.member.model.Member;
-  
+
+@Mapper
+@Repository
 public interface IMemberRepository { 
 	void insertMember(Member member);
 	 Member selectMember(String userid);
